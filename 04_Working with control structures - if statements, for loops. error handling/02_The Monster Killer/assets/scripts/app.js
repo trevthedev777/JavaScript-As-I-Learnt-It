1e5b67ce161107fe830693ee122c978fc40855b0
Trevor Lehmann <trevor@trevorlehmann.com>
2023-01-07 15:49:45 +0000

    Add logic to output message on winning or losing or drawing against opponent

--- 04_Working with control structures - if statements, for loops. error handling/02_The Monster Killer/assets/scripts/app.js
+++ 04_Working with control structures - if statements, for loops. error handling/02_The Monster Killer/assets/scripts/app.js
@@ -1,5 +1,6 @@
 // Global Variables
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -11,6 +12,17 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE); 
+    currentPlayerHealth -= playerDamage;
+
+    // Battle Phase
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You Win!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You Lost!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You Both DEAD!')
+    }
 }
 
 // event listeners
