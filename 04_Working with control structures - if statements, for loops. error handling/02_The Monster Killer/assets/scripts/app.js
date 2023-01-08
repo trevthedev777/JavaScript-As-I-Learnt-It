a6d3647270697c4dd0955dfa1fef917221158c0d
Trevor Lehmann <trevor@trevorlehmann.com>
2023-01-08 14:36:56 +0000

    added the user input values prompt

--- 04_Working with control structures - if statements, for loops. error handling/02_The Monster Killer/assets/scripts/app.js
+++ 04_Working with control structures - if statements, for loops. error handling/02_The Monster Killer/assets/scripts/app.js
@@ -4,7 +4,16 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 10;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt('Maximum life for you and the monster', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+// ! if the user input is NaN then default value is set
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+    chosenMaxLife = 100;
+    alert('Invalid input, default value selected');
+} 
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
